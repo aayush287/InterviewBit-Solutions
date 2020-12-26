602c2fc8fc056ba412517361494ac48d8fe3979d
aayush287 <aayushchaudhary287@gmail.com>
2020-12-26 11:41:03 +0530

    type: flip solution

--- Arrays/flip.py
+++ Arrays/flip.py
@@ -1,5 +1,21 @@
 def flip(A):
     no_of_zeroes = 0
     l = 0
-    r = 0
-    max = 0
+    result = []
+    max_sum = 0
+
+    for i in range(len(A)):
+        if A[i] == '0':
+            no_of_zeroes += 1
+
+            if no_of_zeroes > max_sum:
+                max_sum = no_of_zeroes
+                result = [l+1, i+1]
+        else:
+            no_of_zeroes -= 1
+            if no_of_zeroes < 0:
+                l = i + 1
+                no_of_zeroes = 0
+    return result
+
+print(flip('010'))        
